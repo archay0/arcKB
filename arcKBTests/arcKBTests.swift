59259dff238405c1b1f05753551163db2c86072a
archay wakodikar <102926138+archay0@users.noreply.github.com>
2025-01-11 21:45:58 +0100

    frame

---
+++ arcKBTests/arcKBTests.swift
@@ -0,0 +1,17 @@
+//
+//  arcKBTests.swift
+//  arcKBTests
+//
+//  Created by P01s0n on 11.01.25.
+//
+
+import Testing
+@testable import arcKB
+
+struct arcKBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
